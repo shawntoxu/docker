8e1d2168bbbdac8b2e627ec4a3612fc19bb5fd9c
shawntoxu <comwangxu@gmail.com>
2017-11-23 17:12:32 +0800

    Update start-docker.sh

--- paas/1.2/bin/start-docker.sh
+++ paas/1.2/bin/start-docker.sh
@@ -69,6 +69,14 @@ ip link del docker0 1>/dev/null 2>&1
 if [ -z "$DOCKER_LOG_LEVEL" ]; then
     DOCKER_LOG_LEVEL=warning
 fi
+
+#set host limit 
+ulimit -n 65535
+
+# version > 1.8.3   daemon 
+# add ulimit set 
+#docker daemon  --default-ulimit nofile=65535:65535 --default-ulimit nproc=65535:65535 --bip=${FLANNEL_SUBNET} --mtu=${FLANNEL_MTU} ${insecure_registries} --log-level=${DOCKER_LOG_LEVEL} --storage-driver=${STORAGE} >> $DOCKER_LOG_FILE 2>&1 &
+
 docker -d --bip=${FLANNEL_SUBNET} --mtu=${FLANNEL_MTU} ${insecure_registries} --log-level=${DOCKER_LOG_LEVEL} --storage-driver=aufs >> $DOCKER_LOG_FILE 2>&1 &
 sleep 2
 if ! is_binary_running "docker"; then
